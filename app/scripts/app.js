1c4fa176d69f192fbddf763cd60930ebe2ae039a
michal skula <mskula@way2.pl>
2016-08-10 14:15:21 +0200

    first commit - project start yo generator

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name evoAppApp
+ * @description
+ * # evoAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('evoAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name evoAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the evoAppApp
+ */
+angular.module('evoAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
